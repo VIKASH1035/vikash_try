404a5f68d53ff09b3bb5e1c65c17bc1fe78ca322
VIKASH1035 <vikashsai1198@gmail.com>
2023-12-09 13:59:38 +0530

    second commit.

--- src/main/java/com/example/VikashDemo/demo/DemoApplication.java
+++ src/main/java/com/example/VikashDemo/demo/DemoApplication.java
@@ -9,7 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("I am pushing my first code to github for testing.");
+		System.out.println("I am pushing my second code to github for testing.");
+		System.out.println("Here i will crete my first pr!");
 
 	}
 
